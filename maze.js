2f4237f9df2c0acc024388d0e4d3542046f93773
Kevin Viera <53414453+K-Viera@users.noreply.github.com>
2022-03-21 14:06:44 -0500

    build maze border

--- maze.js
+++ maze.js
@@ -23,6 +23,21 @@ class Maze {
         }
         current = this.grid[0][0];
     }
+
+    draw() {
+        maze.width = this.size;
+        maze.height = this.size;
+        maze.style.background = "black";
+        current.visited = true;
+
+        for (let r = 0; r < this.rows; r++) {
+            for (let c = 0; c < this.columns; c++) {
+                let grid = this.grid;
+                grid[r][c].show(this.size, this.rows, this.columns);
+                grid[r][c].visited = true;
+            }
+        }
+    }
 }
 
 class Cell {
@@ -39,7 +54,48 @@ class Cell {
             bottomWall: true,
         }
     }
+
+    drawTopWall(x, y, size, columns) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x + size / columns, y);
+        ctx.stroke();
+    }
+    drawRighWall(x, y, size, columns, rows) {
+        ctx.beginPath();
+        ctx.moveTo(x + size / columns, y);
+        ctx.lineTo(x + size / columns, y + size / rows);
+        ctx.stroke();
+    }
+    drawBottomWall(x, y, size, columns, rows) {
+        ctx.beginPath();
+        ctx.moveTo(x + size / columns, y + size / rows);
+        ctx.lineTo(x, y + size / rows);
+        ctx.stroke();
+    }
+    drawLeftWall(x, y, size, rows) {
+        ctx.beginPath();
+        ctx.moveTo(x, y + size / rows);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+    show(size, rows, columns) {
+        let x = this.colNum * size / columns;
+        let y = this.rowNum * size / rows;
+        ctx.strokeStyle = "#ffffff";
+        ctx.fillStyle = "black";
+        ctx.lineWidth = 2;
+
+        if (this.walls.topWall) this.drawTopWall(x, y, size, columns);
+        if (this.walls.rigthWall) this.drawRighWall(x, y, size, columns, rows);
+        if (this.walls.bottomWall) this.drawBottomWall(x, y, size, columns, rows);
+        if (this.walls.leftWall) this.drawLeftWall(x, y, size, rows);
+        if (this.visited) {
+            ctx.fillRect(x + 1, y + 1, size / columns - 2, size / rows - 2);
+        }
+    }
 }
 
 let newMaze = new Maze(500, 10, 10);
 newMaze.setup();
+newMaze.draw();
