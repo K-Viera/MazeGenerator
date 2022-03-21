62d6a8a1a38316028edad6bcbfae54878533ff23
Kevin Viera <53414453+K-Viera@users.noreply.github.com>
2022-03-21 13:19:41 -0500

    maze setup

---
+++ maze.js
@@ -0,0 +1,45 @@
+let maze = document.querySelector(".maze");
+let ctx = maze.getContext("2d");
+
+let current;
+
+class Maze {
+    constructor(size, rows, columns) {
+        this.size = size;
+        this.rows = rows;
+        this.columns = columns;
+        this.grid = [];
+        this.stack = [];
+    }
+
+    setup() {
+        for (let r = 0; r < this.rows; r++) {
+            let row = [];
+            for (let c = 0; c < this.columns; c++) {
+                let cell = new Cell(r, c, this.grid, this.size);
+                row.push(cell);
+            }
+            this.grid.push(row);
+        }
+        current = this.grid[0][0];
+    }
+}
+
+class Cell {
+    constructor(rowNum, colNum, parentGrid, parentSize) {
+        this.rowNum = rowNum;
+        this.colNum = colNum;
+        this.parentGrid = parentGrid;
+        this.parentSize = parentSize;
+        this.visited = false;
+        this.walls = {
+            topWall: true,
+            rigthWall: true,
+            leftWall: true,
+            bottomWall: true,
+        }
+    }
+}
+
+let newMaze = new Maze(500, 10, 10);
+newMaze.setup();
